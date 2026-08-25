3570fb87f482bb2215c054c8bcaf832b21fe74c7
edoardo.giacopuzzi <edoardo.giacopuzzi@fht.org>
2026-08-25 12:10:49 +0200

    alice container with PNG graphic support

---
+++ alice_container_png/1.0/install.R
@@ -0,0 +1,66 @@
+library(remotes)
+
+bioc_repos <- c(
+  "https" = "https://cran.r-project.org",
+  "bioc" = "https://bioconductor.org/packages/3.19/bioc",
+  "annotation" = "https://bioconductor.org/packages/3.19/data/annotation",
+  "experiment" = "https://bioconductor.org/packages/3.19/data/experiment"
+)
+
+split_string <- function(input_string) {
+  # Split the string based on the version symbols
+  parts <- strsplit(input_string, "==|<=|>=|<|>")[[1]]
+  
+  # Find the symbol used in the input string
+  symbol <- regmatches(input_string, regexpr("==|<=|>=|<|>", input_string))
+  
+  # Return the parts and the symbol
+  version = "NA"
+  if (length(symbol) > 0) {
+	if (symbol == "==") {
+	  version = parts[2]
+    } else {
+	  version = paste(symbol, parts[2], sep = " ")
+    }
+  }
+  return(list(pkg_name = parts[1], version = version))
+}
+
+
+# Read list of packages from requirements.txt
+pkgs <- readLines("requirements.txt")
+
+message("Found ", length(pkgs), " packages to install")
+
+# Install packages getting version string where present in the format "package version"
+for (pkg in pkgs) {
+	source <- "CRAN"
+	pkg_name <- pkg
+	pkg <- strsplit(pkg, "\\+")[[1]]
+	if (length(pkg) == 2) { 
+		source <- pkg[1] 
+		pkg_name <- pkg[2]
+	}
+
+	if (source == "git") {
+		message("Installing package ", pkg_name, " from ", source)
+		remotes::install_github(pkg_name)
+		next
+	}
+
+	pkg_name <- split_string(pkg_name)
+	if (trimws(pkg_name$version) == "NA") { pkg_name$version <- NULL }
+	message("Installing package ", pkg_name$pkg_name, " with version ", ifelse(is.null(pkg_name$version), "latest", pkg_name$version), " from ", source)
+
+	if (source == "CRAN") {
+		remotes::install_version(pkg_name$pkg_name, version=pkg_name$version, upgrade="never", repos=c("https://cloud.r-project.org/"))
+	}
+	if (source == "bioc") {
+		remotes::install_version(pkg_name$pkg_name, version=pkg_name$version, upgrade="never", repos=bioc_repos)
+	}
+
+	if ( ! library(pkg_name$pkg_name, character.only=TRUE, logical.return=TRUE) ) {
+    	quit(status=1, save='no')
+    }
+
+}
